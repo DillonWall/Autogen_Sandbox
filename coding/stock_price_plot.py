41cc60c39f5b0fcaefd49e0c952252a8c6dac356
Dillon Wall <DillonMWall@msn.com>
2023-09-28 22:39:02 -0700

    Update stock_price_plot.py

--- coding/stock_price_plot.py
+++ coding/stock_price_plot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Downloading the stock data
 nvda_data = yf.download('NVDA', start='2022-01-01')
-tesla_data = yf.download('TESLA', start='2022-01-01')
+tesla_data = yf.download('TSLA', start='2022-01-01')  # Corrected the ticker symbol here
 
 # Calculating the daily percentage change
 nvda_data['Pct Change'] = nvda_data['Adj Close'].pct_change()
@@ -17,10 +17,10 @@ plt.figure(figsize=(14,7))
 # Plotting NVDA data
 plt.plot(nvda_data.index, nvda_data['Pct Change'], label='NVDA')
 
-# Plotting TESLA data
-plt.plot(tesla_data.index, tesla_data['Pct Change'], label='TESLA')
+# Plotting TSLA data
+plt.plot(tesla_data.index, tesla_data['Pct Change'], label='TSLA')  # Corrected the label here
 
-plt.title('NVDA vs TESLA Stock Price Change YTD')
+plt.title('NVDA vs TSLA Stock Price Change YTD')  # Corrected the title here
 plt.xlabel('Date')
 plt.ylabel('Percentage Change')
 plt.legend()
